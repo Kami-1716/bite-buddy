b8acf3b9c9da0858cbcb846d2c496edab1b034a5
Muhammad Kamran <134825430+Kami-1716@users.noreply.github.com>
2024-02-29 00:22:47 +0500

    Add Auth0 authentication and mobile navigation links

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -8,22 +8,37 @@ import {
 } from "./ui/sheet";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from "./MobileNavLinks";
 
 const MobileNav = () => {
+  const { loginWithRedirect, isAuthenticated, user } = useAuth0();
+
   return (
     <Sheet>
       <SheetTrigger>
         <Menu className="text-orange-500" />
       </SheetTrigger>
-      <SheetContent className="flex space-y-4 flex-col">
+      <SheetContent className="flex  flex-col">
         <SheetTitle>
-          <span>Welcome to BiteBuddy.com!</span>
+          {isAuthenticated ? (
+            `Welcome, ${user?.name}`
+          ) : (
+            <span>Welcome to BiteBuddy.com!</span>
+          )}
         </SheetTitle>
         <Separator />
-        <SheetDescription className="flex">
-          <Button className="flex-1 font-bold hover:bg-orange-500">
-            Login
-          </Button>
+        <SheetDescription className="flex flex-col gap-3">
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button
+              className="flex-1 font-bold hover:bg-orange-500"
+              onClick={async () => await loginWithRedirect()}
+            >
+              Login
+            </Button>
+          )}
         </SheetDescription>
       </SheetContent>
     </Sheet>
