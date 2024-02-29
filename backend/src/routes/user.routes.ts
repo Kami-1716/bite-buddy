f9abed36a6a619f327d7082c913cb3f02c10acbc
Muhammad Kamran <134825430+Kami-1716@users.noreply.github.com>
2024-03-01 00:07:41 +0500

    complete auth functionality including access token

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { createNewUser } from "../controllers/user.controller";
+import { jwtCheck } from "../middlewares/auth0.middleware";
 
 const router = express.Router();
 
-router.route("/").post(createNewUser);
+router.route("/").post(jwtCheck, createNewUser);
 
 export default router;
