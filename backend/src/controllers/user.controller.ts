0bfa05ad78be5a267d24559c889f53a7aaba5293
Muhammad Kamran <134825430+Kami-1716@users.noreply.github.com>
2024-03-01 01:06:04 +0500

    Add express-validator and jsonwebtoken dependencies, update user.routes.ts to include updateUser route, add validation.middleware.ts, and update auth0.middleware.ts for token parsing

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -18,4 +18,27 @@ export const createNewUser = async (req: Request, res: Response) => {
   }
 };
 
-export default { createNewUser };
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { name, addressLine1, country, city } = req.body;
+
+    const user = await User.findById(req.userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.country = country;
+    user.city = city;
+    await user.save();
+    res.status(200).json(user.toObject());
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error while updating the user" });
+  }
+};
+
+export default {
+  createNewUser,
+  updateUser,
+};
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { createNewUser } from "../controllers/user.controller";
-import { jwtCheck } from "../middlewares/auth0.middleware";
+import { createNewUser, updateUser } from "../controllers/user.controller";
+import { jwtCheck, jwtParse } from "../middlewares/auth0.middleware";
+import { validateMyUserRequest } from "../middlewares/validation.middleware";
 
 const router = express.Router();
 
 router.route("/").post(jwtCheck, createNewUser);
+router.route("/").put(jwtCheck, jwtParse, validateMyUserRequest, updateUser);
 
 export default router;
